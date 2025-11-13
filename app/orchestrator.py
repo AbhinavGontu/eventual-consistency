33c8fb79014f9f1698db60ec41d8f3f9e9de87ba
Abhinav Gontu <abhinavgontu@gmail.com>
2025-11-13 21:34:12 +0530

    refactor: Define abstract SagaStep interface

--- app/orchestrator.py
+++ app/orchestrator.py
@@ -1,3 +1,6 @@
 class SagaOrchestrator:
     def __init__(self):
         self.steps = []
+class SagaStep:
+    async def execute(self, context): raise NotImplementedError
+    async def compensate(self, context): raise NotImplementedError
